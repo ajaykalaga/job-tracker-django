56c72518785aab8edc100de6120440fe243eedbc
Ajay Sharma <ajayskalaga@gmail.com>
2026-02-01 16:03:24 +0530

    Dashboard template consuming DRF API

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from .models import JobApplications
+from .serializers import JobApplicationSerializer
+from rest_framework import generics,permissions
 # Create your views here.
+
+class JobListCreateView(generics.ListCreateAPIView):
+  serializer_class = JobApplicationSerializer
+  permissions_classes = [permissions.IsAuthenticated]
+  def get_queryset(self):
+    return JobApplications.objects.filter(user=self.request.user)
+  
+  def perform_create(self,serializer):
+    serializer.save(user=self.request.user)
